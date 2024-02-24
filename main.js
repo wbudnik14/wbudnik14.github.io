3b2b671e781860307b6581f87c80f604f37326a9
wbudnik14 <132603351+wbudnik14@users.noreply.github.com>
2024-02-24 18:49:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-// ----menu hamburger----
-const menu_btn = document.querySelector(".hamburger");
-
-menu_btn.addEventListener("click", function () {
-  menu_btn.classList.toggle("is-active");
+//dropdown hide
+// show/hide the menu when examples is clicked
+$(".dropdowntoggle").on("click", function () {
+  $(".nav__links").toggle();
 });
 
-const menuHamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const textBox = document.querySelector(".text-box");
-
-menuHamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("mobile-menu");
-  textBox.classList.toggle("text-menu");
+// hide the menu when an exmple is clicked
+$(".example").on("click", function () {
+  $(".nav__links").hide();
 });
 
+//zamykanie produktów
+$(document).ready(function () {
+  // Obsługa zdarzenia kliknięcia na desktopach
+  $(".products").on("click", function (event) {
+    // Jeśli jesteśmy na urządzeniu mobilnym, zapobiegamy domyślnej akcji kliknięcia, aby uniknąć podwójnego zdarzenia
+    if ($(window).width() > 768) {
+      event.preventDefault();
+    }
+    // Toggle klasy show dla .dropdown
+    $(".dropdown").toggleClass("show");
+  });
+
+  // Obsługa zdarzenia dotyku na urządzeniach mobilnych
+  $(".products").on("touchstart", function () {
+    // Toggle klasy show dla .dropdown
+    $(".dropdown").toggleClass("show");
+  });
+
+  // Funkcja do zamykania .dropdown po kliknięciu na dowolny link wewnątrz
+  $(".dropdown a").on("click", function () {
+    $(".dropdown").removeClass("show");
+  });
+});
 // Funkcja do przewijania do wybranego elementu
 function scrollToElement(elementId) {
   var targetElement = document.getElementById(elementId);
